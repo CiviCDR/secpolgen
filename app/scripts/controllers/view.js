d62b3bab0a0ec035280ea21cd86e887be17277eb
meg <meg@admin-ms-MacBook-Pro.local>
2017-02-02 05:28:20 +0000

    auto update org name

--- app/scripts/controllers/view.js
+++ app/scripts/controllers/view.js
@@ -6,10 +6,10 @@ var ViewCtrl = angularApp.controller('ViewCtrl', function ($scope, FormService, 
     // read form with given id
     FormService.form($routeParams.id).then(function(form) {
         $scope.form = form;
+        $scope.org_name = '';
+        // accordion settings
+        $scope.accordion = {}
+        $scope.accordion.oneAtATime = true;
     });
-
-    // accordion settings
-    $scope.accordion = {}
-    $scope.accordion.oneAtATime = true;
 
 });
